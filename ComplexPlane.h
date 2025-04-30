7947a9a3c93cdf477eab10d893324b3ebd12821d
khalid286 <w2092006@apps.losrios.edu>
2025-04-29 21:17:26 -0700

    Add files via upload

---
+++ ComplexPlane.h
@@ -0,0 +1,47 @@
+// ComplexPlane.h
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <sstream>
+
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0f;
+const float BASE_HEIGHT = 4.0f;
+const float BASE_ZOOM = 0.5f;
+
+enum class State {
+    CALCULATING,
+    DISPLAYING
+};
+
+class ComplexPlane : public sf::Drawable {
+public:
+    ComplexPlane(int pixelWidth, int pixelHeight);
+
+    void updateRender();
+    void zoomIn();
+    void zoomOut();
+    void setCenter(sf::Vector2i mousePixel);
+    void setMouseLocation(sf::Vector2i mousePixel);
+    void loadText(sf::Text& text);
+
+private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+    sf::Vector2f mapPixelToCoords(sf::Vector2i pixel) const;
+    size_t countIterations(sf::Vector2f coord) const;
+    void iterationsToRGB(size_t count, sf::Uint8& r, sf::Uint8& g, sf::Uint8& b) const;
+
+    int m_pixelWidth;
+    int m_pixelHeight;
+    float m_aspectRatio;
+
+    sf::Vector2f m_plane_center;
+    sf::Vector2f m_plane_size;
+    sf::Vector2f m_mouseLocation;
+
+    int m_zoomCount;
+    State m_State;
+
+    sf::VertexArray m_vArray;
+};
+
