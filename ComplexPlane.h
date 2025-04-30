98dc4f7caf74b882608484600d108215f978e771
Michael Sundukos <w1733924@apps.losrios.edu>
2025-04-29 22:36:46 -0700

    Added some include statements for ComplexPlane.h

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -1,47 +1,42 @@
-// ComplexPlane.h
+#pragma once
 #include <SFML/Graphics.hpp>
-#include <vector>
+#include <SFML/Audio.hpp>
+#include <iostream>
 #include <sstream>
+#include <complex>
+#include <cmath>
 
 const unsigned int MAX_ITER = 64;
-const float BASE_WIDTH = 4.0f;
-const float BASE_HEIGHT = 4.0f;
-const float BASE_ZOOM = 0.5f;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
 
-enum class State {
-    CALCULATING,
-    DISPLAYING
-};
+enum class State {CALCULATING,DISPLAYING};
 
-class ComplexPlane : public sf::Drawable {
+class ComplexPlane : public sf::Drawable
+{
 public:
-    ComplexPlane(int pixelWidth, int pixelHeight);
-
-    void updateRender();
-    void zoomIn();
-    void zoomOut();
-    void setCenter(sf::Vector2i mousePixel);
-    void setMouseLocation(sf::Vector2i mousePixel);
-    void loadText(sf::Text& text);
+	//public member functions
+	ComplexPlane(int pixelWidth, int pixelHeight);
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	void zoomIn();
+	void zoomOut();
+	void setCenter(sf::Vector2i mousePixel);
+	void loadText(sf::Text& text);
+	void updateRender();
 
 private:
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-
-    sf::Vector2f mapPixelToCoords(sf::Vector2i pixel) const;
-    size_t countIterations(sf::Vector2f coord) const;
-    void iterationsToRGB(size_t count, sf::Uint8& r, sf::Uint8& g, sf::Uint8& b) const;
-
-    int m_pixelWidth;
-    int m_pixelHeight;
-    float m_aspectRatio;
-
-    sf::Vector2f m_plane_center;
-    sf::Vector2f m_plane_size;
-    sf::Vector2f m_mouseLocation;
-
-    int m_zoomCount;
-    State m_State;
-
-    sf::VertexArray m_vArray;
+	//private member variables
+	sf::VertexArray m_vArray;
+	State m_State;
+	sf::Vector2f m_mouseLocation;
+	sf::Vector2i m_pixel_size;
+	sf::Vector2f m_plane_center;
+	sf::Vector2f m_plane_size;
+	int m_zoomCount;
+	float m_aspectRatio;
+	//private member functions
+	size_t countIterations(sf::Vector2f coord);
+	void iterationsToRGB(size_t count, sf::Uint8& r, sf::Uint8& g, sf::Uint8& b);
+	sf::Vector2f mapPixelToCoords(sf::Vector2i mousePixel);
 };
-
